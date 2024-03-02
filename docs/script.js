cc870d9c74d5aed98ad2fe88b3dce349bee3a1bb
Salini <iamsalinivijayan@gmail.com>
2024-03-02 22:03:33 +0530

    Initial commit

---
+++ docs/script.js
@@ -0,0 +1,13 @@
+
+
+var navLinks=document.getElementById("nav-links");
+
+function showMenu(){
+    navLinks.style.right="0";
+}
+
+var navLinks=document.getElementById("nav-links");
+function hideMenu(){
+    navLinks.style.right="-200px";
+}
+
